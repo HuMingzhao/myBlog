24cd98a8791af1547f61edfbf434ded653133fd9
Hu Mingzhao <mingzhao@yaitoo.cn>
2020-08-18 11:38:30 +0800

    change resume style

--- src/views/Resume/components/resumeContent.vue
+++ src/views/Resume/components/resumeContent.vue
@@ -1,16 +1,36 @@
 <template>
     <main class="resume-content bg-light-gray">
       <section>
-          <h1>123</h1>
+          <h1>Information</h1>
           <div class="boxes">
             <el-row :gutter="10" class="bg-light-white">
-                <el-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1">111</el-col>
-                <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="11">222</el-col>
-                <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="11">333</el-col>
-                <el-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1">555</el-col>
+                <el-col :xs="24" :sm="8" :md="8" :lg="8" :xl="8">sugar|male|1993</el-col>
+                <el-col :xs="24" :sm="8" :md="8" :lg="8" :xl="8">bachelor|NanChangeHnagkong|NDT</el-col>
+                <el-col :xs="24" :sm="8" :md="8" :lg="8" :xl="8">web Engineer|1 year</el-col>
+            </el-row>
+            <el-row :gutter="10" class="bg-light-white">
+                <el-col :xs="24" :sm="8" :md="8" :lg="8" :xl="8">expect position: web Engineer</el-col>
+                <el-col :xs="24" :sm="16" :md="16" :lg="16" :xl="16">expect city: Xiamen</el-col>
+            </el-row>
+            <el-row :gutter="10" class="bg-light-white">
+                <el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24">Github: ：<a target="_blank" href="https://github.com/HuMingzhao">https://github.com/HuMingzhao</a></el-col>
+            </el-row>
+            <el-row :gutter="10" class="bg-light-white">
+                <el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24">personal site: ：<a target="_blank" href="#">...</a></el-col>
             </el-row>
           </div>
       </section>
+
+      <section>
+        <h1>Contact</h1>
+        <div class="boxes">
+          <el-row :gutter="10" class="bg-light-white">
+              <el-col :xs="24" :sm="8" :md="8" :lg="8" :xl="8">phone: XXXXXXXX</el-col>
+              <el-col :xs="24" :sm="8" :md="8" :lg="8" :xl="8">Email: XXXX</el-col>
+              <el-col :xs="24" :sm="8" :md="8" :lg="8" :xl="8">Personal site: XXXX</el-col>
+            </el-row>
+        </div>
+    </section>
     </main>
   </template>
 
@@ -46,6 +66,9 @@ export default {
         .boxes {
             border-radius: 5px;
             margin-bottom: 20px;
+        }
+        .el-row {
+            padding: 10px 15px;
         }
       }
       .bg-light-gray {
