3b2df56b8d3dfc8796a8faaa000371e3a06cae3c
Hu Mingzhao <mingzhao@yaitoo.cn>
2020-08-24 16:03:45 +0800

    change carousel height

--- src/views/Home/components/middleContent.vue
+++ src/views/Home/components/middleContent.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="middle-content-wrapper">
     <div class="carousel-wrapper">
-      <el-carousel indicator-position="none" height="509px" :autoplay="false">
+      <el-carousel ref="carousel" indicator-position="none" :height="carouselHeight + 'px'" :autoplay="false">
         <el-carousel-item v-for="item in 4" :key="item">
-          <h3 style="height: 509px;background-color: gray;">{{ item }}</h3>
+          <h3 style="height: 100%;background-color: gray;">{{ item }}</h3>
         </el-carousel-item>
       </el-carousel>
     </div>
@@ -17,6 +17,24 @@ export default {
   name: 'middleContent',
   components: {
     card
+  },
+  data () {
+    return {
+      carouselHeight: ''
+    }
+  },
+  mounted () {
+    //  509 / 906
+    this.carouselHeight = Math.round(this.$refs.carousel.$el.clientWidth * 0.56)
+    window.addEventListener('resize', this.getCarouselHeight)
+  },
+  methods: {
+    getCarouselHeight () {
+      this.carouselHeight = Math.round(this.$refs.carousel.$el.clientWidth * 0.56)
+    }
+  },
+  destroyed () {
+    window.removeEventListener('resize', this.getCarouselHeight)
   }
 }
 </script>
